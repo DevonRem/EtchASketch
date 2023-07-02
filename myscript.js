460d3b77701267e11a356b6ad6ec1ac9478f3009
DevonRem <DevonReminick@gmail.com>
2023-07-02 15:42:14 -0400

    Create grid

--- myscript.js
+++ myscript.js
@@ -1,6 +1,18 @@
 const container = document.querySelector('.container');
-const grid = document.createElement('div');
-grid.classList.add('grid');
-container.appendChild(grid);
+
 //probably turn this into a function later: createGrid()
 
+function createGrid(num) {
+    for (let rows = 0; rows < num; rows++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let columns = 0; columns < num; columns++) {
+            const col = document.createElement('div');
+            col.classList.add('col');
+            row.appendChild(col);
+        }
+    }
+}
+
+createGrid(16);
